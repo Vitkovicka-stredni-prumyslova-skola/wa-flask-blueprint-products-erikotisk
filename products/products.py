0ccc7d8a1b9ba128663aef341947f8fca8c0359d
Erik Otisk <erik.otisk@student.vitkovickastredni.cz>
2024-02-22 13:17:31 +0100

    CV-Flask-related-products

--- products/products.py
+++ products/products.py
@@ -9,10 +9,29 @@ def index():
     data = GetAllProducts()
     l = len(data)
     
+
+
     return render_template('products/products.html', length = l, products = data)
 
 @products_bp.route('/products/<int:id>')
 def detailOfProduct(id):
     data = GetSingleProducts(id)
 
-    return render_template('products/detail.html', detailOfPorduct = data)
+    dataS = GetAllProducts()
+
+    wantedData = []
+
+    a = 0
+
+    for item in dataS:
+        if (item['category'] == "women's clothing" and a < 4):
+            wantedData.append[item]
+            a += 1
+
+    wantedLen = len(wantedData)
+
+    print(wantedLen)
+    print(wantedData)
+
+
+    return render_template('products/detail.html', detailOfPorduct = data, wantedLen = wantedLen, wantedData = wantedData)
